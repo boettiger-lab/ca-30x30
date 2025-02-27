fa174eacfb05f0906ce2cc5b1e1abd359c039b5e
Cassie Buhler <cassidy.buhler@gmail.com>
2025-02-27 00:38:28 +0000

    NRP model

--- app/app.py
+++ app/app.py
@@ -148,8 +148,10 @@ with open('app/system_prompt.txt', 'r') as file:
 
 from langchain_openai import ChatOpenAI
     
-llm = ChatOpenAI(model = "kosbu/Llama-3.3-70B-Instruct-AWQ", api_key="cirrus-vllm-secret-api-key", base_url = "https://llm.cirrus.carlboettiger.info/v1/",  temperature=0)
+# llm = ChatOpenAI(model = "kosbu/Llama-3.3-70B-Instruct-AWQ", api_key="cirrus-vllm-secret-api-key", base_url = "https://llm.cirrus.carlboettiger.info/v1/",  temperature=0)
 # llm = ChatOpenAI(model="gpt-4", temperature=0)
+llm = ChatOpenAI(model = "llama3", api_key=st.secrets['NRP_API_KEY'], base_url = "https://llm.nrp-nautilus.io/",  temperature=0)
+
 
 managers = ca.sql("SELECT DISTINCT manager FROM mydata;").execute()
 names = ca.sql("SELECT name FROM mydata GROUP BY name HAVING SUM(acres) >10000;").execute()
@@ -181,7 +183,6 @@ def run_sql(query,color_choice):
     output = few_shot_structured_llm.invoke(query)
     sql_query = output.sql_query
     explanation =output.explanation
-
     if not sql_query: # if the chatbot can't generate a SQL query.
         st.success(explanation)
         return pd.DataFrame({'id' : []})
@@ -272,6 +273,8 @@ with st.container():
                                 key: (True if key in cols else value) 
                                 for key, value in chatbot_toggles.items()
                             }
+                        for key, value in chatbot_toggles.items():
+                            st.session_state[key] = value  # Update session state
                     else:
                         ids = []
         except Exception as e:
