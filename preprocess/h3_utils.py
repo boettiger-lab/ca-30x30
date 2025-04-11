8419f191171e6f9f712774b17b3175e86540eb37
Cassie Buhler <cassidy.buhler@gmail.com>
2025-04-11 21:12:41 +0000

    removing h3 code, will revisit later
    later: fix h3 to raster, process h3 by looping.

--- preprocess/h3_utils.py
+++ preprocess/h3_utils.py
@@ -1,14 +1,13 @@
 from utils import *
 import re
 
-def convert_h3(con, s3, folder, file, cols, zoom):
+def convert_h3(con, s3, folder, file, cols, zoom, base_folder = "CBN/"):
     """
     Driver function to convert geometries to h3.
     If no zoom levels exist -> compute from geometry at target zoom.
-    If lower zoom exists -> compute children from max available until target zoom.
     """
     cols = ", ".join(cols) if isinstance(cols, list) else cols
-    bucket, path = info(folder, file)
+    bucket, path = info(folder, file, base_folder)
     path, file = os.path.split(path)
     name, ext = os.path.splitext(file)
     name = name.replace('-', '')
@@ -23,7 +22,7 @@ def convert_h3(con, s3, folder, file, cols, zoom):
             zooms.append(int(match.group(1)))
             
     if not zooms: # if no h3 files exist
-        print(f'No h3 files exists, computing {zoom} from geometry.')
+        print(f'No h3 files exists, computing zoom level {zoom} from geometry.')
         con.read_parquet(f"s3://{bucket}/{path}/{file}", table_name=name)
         h3_from_geom(con, name, cols, zoom)
         con.sql(f'''
@@ -38,21 +37,21 @@ def convert_h3(con, s3, folder, file, cols, zoom):
             print(f'Zoom {zoom} already exists!')
             return 
             
-        elif current_zoom < zoom: #compute child of most refined zoom level
-            print(f'Reading zoom {current_zoom}')
-            con.read_parquet(
-                f"s3://{bucket}/{path}/hex/zoom{current_zoom}/{name}.parquet",
-                table_name=f"h3_h{current_zoom}"
-            )
-            print(f'Computing {zoom} from {current_zoom}')
+        # elif current_zoom < zoom: #compute child of most refined zoom level
+        #     print(f'Reading zoom {current_zoom}')
+        #     con.read_parquet(
+        #         f"s3://{bucket}/{path}/hex/zoom{current_zoom}/{name}.parquet",
+        #         table_name=f"h3_h{current_zoom}"
+        #     )
+        #     print(f'Computing {zoom} from {current_zoom}')
             
-            for z in range(current_zoom + 1, zoom + 1):
-                print(f'Current zoom {z}')
-                h3_from_parent(con, z)
-                con.sql(f'''
-                    SELECT *, UNNEST(h3_cell_to_children(h{z-1}, {z})) AS h{z}
-                    FROM h3_h{z-1}
-                ''').to_parquet(f"s3://{bucket}/{path}/hex/zoom{z}/{name}.parquet")
+        #     for z in range(current_zoom + 1, zoom + 1):
+        #         print(f'Current zoom {z}')
+        #         h3_from_parent(con, z)
+        #         con.sql(f'''
+        #             SELECT *, UNNEST(h3_cell_to_children(h{z-1}, {z})) AS h{z}
+        #             FROM h3_h{z-1}
+        #         ''').to_parquet(f"s3://{bucket}/{path}/hex/zoom{z}/{name}.parquet")
   
     
 def h3_from_geom(con, name, cols, zoom):
@@ -60,20 +59,19 @@ def h3_from_geom(con, name, cols, zoom):
     Computes hexes directly from geometry.
     """
     con.raw_sql(f'''
-        CREATE OR REPLACE TEMP TABLE t2 AS
-        WITH t1 AS (
-            SELECT {cols}, ST_Dump(geom) AS geom 
-            FROM {name}
-        )
-        SELECT {cols},
-               h3_polygon_wkt_to_cells_string(ST_Force2D(UNNEST(geom).geom), {zoom}) AS h{zoom}
-        FROM t1
+    CREATE OR REPLACE TEMP TABLE t2 AS
+    SELECT {cols},
+           h3_polygon_wkt_to_cells_string(ST_Force2D(dump.geom), {zoom}) AS h{zoom}
+    FROM (
+        SELECT {cols}, UNNEST(ST_Dump(geom)) AS dump
+        FROM {name}
+    )
     ''')
 
 
-def h3_from_parent(con, zoom):
-    con.raw_sql(f'''
-        CREATE OR REPLACE TEMP TABLE h3_h{zoom} AS
-        SELECT *, UNNEST(h3_cell_to_children(h{zoom-1}, {zoom})) AS h{zoom}
-        FROM h3_h{zoom-1}
-    ''')
+# def h3_from_parent(con, zoom):
+#     con.raw_sql(f'''
+#         CREATE OR REPLACE TEMP TABLE h3_h{zoom} AS
+#         SELECT *, UNNEST(h3_cell_to_children(h{zoom-1}, {zoom})) AS h{zoom}
+#         FROM h3_h{zoom-1}
+#     ''')
--- preprocess/utils.py
+++ preprocess/utils.py
@@ -10,48 +10,50 @@ import ibis
 from ibis import _
 
 import rasterio
-from rasterio.features import shapes
-from shapely.geometry import shape
+from rasterio.transform import xy
+from shapely.geometry import Point
 import numpy as np
+from pyproj import Transformer
 
-
-def info(folder, file, bucket = "public-ca30x30", base_folder = 'CBN/'):
+def info(folder, file, base_folder, bucket = "public-ca30x30"):
     """
     Extract minio path to upload/download data 
     """
-    path = os.path.join(base_folder, folder, file)
+    if (folder is None) & (base_folder is None):
+        path = file
+    else:
+        path = os.path.join(base_folder, folder, file)
     # path = os.path.join(folder, file)
     return bucket, path 
     
-def download(s3, folder, file, file_name = None):
+def download(s3, folder, file, file_name = None, base_folder = "CBN/"):
     """
     Downloading file from minio
     """
     if not file_name: 
         file_name = file
-    bucket, path = info(folder, file)
-    s3.fget_object(bucket, path ,file_name) 
+    bucket, path = info(folder, file, base_folder)
+    s3.fget_object(bucket, path , file_name) 
     return
 
-def upload(s3, folder, file):
+def upload(s3, folder, file, base_folder = "CBN/"):
     """
     Uploading file from minio
     """
-    bucket, path = info(folder, file)
+    bucket, path = info(folder, file, base_folder)
     s3.fput_object(bucket, path ,file) 
     return
 
-def unzip(s3, folder, file):
+def unzip(s3, folder, file, base_folder = "CBN/"):
     """
     Unzipping zip files 
     """
-    download(s3, folder, file)
+    download(s3, folder, file, base_folder)
     with zipfile.ZipFile(file, 'r') as zip_ref:
         zip_ref.extractall()
     return 
 
-# def process_vector(s3, folder, file, file_name = None, gdf = None, crs="EPSG:3310"):
-def process_vector(s3, folder, file, file_name = None, gdf = None, crs="EPSG:4326"):
+def process_vector(s3, folder, file, file_name = None, gdf = None, crs="EPSG:4326", base_folder = "CBN/"):
     """
     Driver function to process vectors 
     """
@@ -67,11 +69,11 @@ def process_vector(s3, folder, file, file_name = None, gdf = None, crs="EPSG:432
     name, ext = os.path.splitext(file)
     parquet_file = f"{name}{'.parquet'}"
     gdf.to_parquet(parquet_file)
-    upload(s3, folder, parquet_file)
+    upload(s3, folder, parquet_file, base_folder)
+    # return gdf.drop('geom',axis = 1).columns.to_list()
+    return
 
-    return gdf.drop('geom',axis = 1).columns.to_list()
-
-def process_raster(s3, folder, file, file_name = None):
+def process_raster(s3, folder, file, file_name = None, base_folder = "CBN/"):
     """
     Driver function to process rasters 
     """
@@ -82,29 +84,30 @@ def process_raster(s3, folder, file, file_name = None):
     output_cog_file = f"{name}_processed_COG{ext}"
     output_vector_file = f"{name}_processed.parquet"
     # Reproject raster
-    if not exists_on_s3(s3, folder, output_file):
+    if not exists_on_s3(s3, folder, output_file, base_folder):
         output_file = reproject_raster(file)
-        upload(s3, folder, output_file)
+        upload(s3, folder, output_file, base_folder)
     else:
         print(f"{output_file} already exists on S3, skipping reprojection/upload.")
 
     # Make COG
-    if not exists_on_s3(s3, folder, output_cog_file):
+    if not exists_on_s3(s3, folder, output_cog_file, base_folder):
         output_cog_file = make_cog(output_file)
-        upload(s3, folder, output_cog_file)
+        upload(s3, folder, output_cog_file, base_folder)
     else:
         print(f"{output_cog_file} already exists on S3, skipping COG conversion/upload.")
 
-    # Vectorize raster
-    if not exists_on_s3(s3, folder, output_vector_file):
-        output_vector_file, cols = make_vector(output_file)
-        upload(s3, folder, output_vector_file)
-    else:
-        print(f"{output_vector_file} already exists on S3, skipping vectorization/upload.")
-        # We still need column names
-        gdf = gpd.read_parquet(output_vector_file)
-        cols = gdf.drop('geom', axis=1).columns.to_list()
-    return cols
+    # # Vectorize raster
+    # if not exists_on_s3(s3, folder, output_vector_file, base_folder):
+    #     output_vector_file, cols = make_vector(output_file)
+    #     upload(s3, folder, output_vector_file, base_folder)
+    # else:
+    #     print(f"{output_vector_file} already exists on S3, skipping vectorization/upload.")
+    #     # We still need column names
+    #     gdf = gpd.read_parquet(output_vector_file)
+    #     cols = gdf.drop('geom', axis=1).columns.to_list()
+    # return cols
+    return 
     
 def reproject_raster(input_file, crs="EPSG:3310"):
     """
@@ -147,31 +150,37 @@ def make_cog(input_file, crs="EPSG:4326"):
         print(f"Error occurred during processing: {e}")
     return output_file 
 
-def make_vector(input_file, crs="EPSG:4326"):
-    """
-    Converting rasters to vector formats in order to convert to h3
-    """
-    name, ext = os.path.splitext(input_file)
-    output_file = f"{name}.parquet"
-    # Open raster
-    with rasterio.open(input_file) as src:
-        image = src.read(1)  # read first band
-        mask = image != src.nodata  # mask out nodata
+# def make_vector(input_file, crs="EPSG:4326"):
+#     """
+#     Converting rasters to vector formats in order to convert to h3
+#     """
+#     name, ext = os.path.splitext(input_file)
+#     output_file = f"{name}.parquet"
     
-        results = (
-            {"geom": shape(geom), "value": value}
-            for geom, value in shapes(image, mask=mask, transform=src.transform)
-        )
-    
-    gdf = gpd.GeoDataFrame.from_records(results)
-    gdf.set_geometry('geom', inplace=True)
-    gdf['id'] = np.arange(len(gdf))
-    gdf.set_crs(src.crs, inplace=True)
-    if gdf.crs != crs:
-        gdf.to_crs(crs, inplace=True)
+#     with rasterio.open(input_file) as src:
+#         band = src.read(1)  # read first band
+#         mask = band != src.nodata  # mask out nodata
+#         rows, cols = np.where(mask)
+#         x, y = rasterio.transform.xy(src.transform, rows, cols, offset = "center")
 
-    gdf.to_parquet(output_file)
-    return output_file, gdf.drop('geom',axis = 1).columns.to_list()
+#         # reproject 
+#         if src.crs and src.crs.to_string() != crs:
+#             transformer = Transformer.from_crs(src.crs, crs, always_xy=True)
+#             x, y = transformer.transform(x, y)
+#             crs_out = crs
+#         else:
+#             crs_out = src.crs
+
+#         gdf = gpd.GeoDataFrame(
+#             {"value": band[rows, cols]},
+#             geometry=[Point(xy) for xy in zip(x, y)],
+#             crs=crs_out
+#         )
+
+#     gdf.rename_geometry('geom', inplace=True)
+#     gdf['id'] = np.arange(len(gdf))
+#     gdf.to_parquet(output_file)
+#     return output_file, gdf.drop('geom',axis = 1).columns.to_list()
 
 def filter_raster(s3, folder, file, percentile):
     """
@@ -192,11 +201,11 @@ def filter_raster(s3, folder, file, percentile):
     profile.update(dtype=rasterio.float64)
     with rasterio.open(new_file, "w", **profile) as dst:
         dst.write(filtered, 1)
-    cols = process_raster(s3, folder, file)
-    return cols
+    process_raster(s3, folder, file)
+    # return cols
+    return
 
-
-def convert_pmtiles(con, s3, folder, file):
+def convert_pmtiles(con, s3, folder, file, base_folder = "CBN/"):
     """
     Convert to PMTiles with tippecanoe 
     """
@@ -205,14 +214,14 @@ def convert_pmtiles(con, s3, folder, file):
             (con.read_parquet(file).execute().set_crs('epsg:3310')
              .to_crs('epsg:4326').to_file(name+'.geojson'))
     to_pmtiles(name+'.geojson', name+'.pmtiles', options = ['--extend-zooms-if-still-dropping'])
-    upload(s3, folder, name+'.pmtiles')
+    upload(s3, folder, name+'.pmtiles', base_folder)
     return
 
-def exists_on_s3(s3, folder, file):
+def exists_on_s3(s3, folder, file, base_folder = "CBN/"):
     """
     Check if a file exists on S3
     """
-    bucket, path = info(folder, file)
+    bucket, path = info(folder, file, base_folder)
     try:
         s3.stat_object(bucket, path)
         return True
